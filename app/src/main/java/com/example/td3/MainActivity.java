7da83b7b5ef7be625e3e99dbda48ab0c730165e9
Nassim <nassimelalam@gmail.com>
2020-09-29 14:23:14 +0200

    enh: exercice 3

--- app/src/main/java/com/example/td3/MainActivity.java
+++ app/src/main/java/com/example/td3/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
      Button lancement_de;
      TextView affichage_de;
      TextView deuxieme_de;
+     EditText nombre_face;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         affichage_de = findViewById(R.id.affiche_valeur_de);
         deuxieme_de = findViewById(R.id.deuxieme_de);
+        nombre_face = findViewById(R.id.nombre_face);
 
         lancement_de = findViewById(R.id.button_lacement_de);
         lancement_de.setOnClickListener(new View.OnClickListener() {
@@ -30,12 +33,20 @@ public class MainActivity extends AppCompatActivity {
                         "De lance",
                         Toast.LENGTH_SHORT);
                 toast.show();
+                String nb_face = nombre_face.getText().toString();
+                if(nb_face.equals("")){
+                     Toast.makeText(MainActivity.this,
+                            "Entrer un nombre de Face",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    int rand1 = rollDice(Integer.parseInt(nb_face));
+                    affichage_de.setText(Integer.toString(rand1));
 
-                int rand1 = (int)(6.0 * Math.random()) + 1;
-                affichage_de.setText(Integer.toString(rollDice(6)));
+                    int rand2 = rollDice(Integer.parseInt(nb_face));
+                    deuxieme_de.setText(Integer.toString(rand2));
+                }
 
-                int rand2 = (int)(6.0 * Math.random()) + 1;
-                deuxieme_de.setText(Integer.toString(rollDice(6)));
 
 
             }
