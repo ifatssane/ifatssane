2eae0c78c121ebb42dddf6ee89e2584ff9e69937
Nassim <nassimelalam@gmail.com>
2020-09-29 14:06:26 +0200

    enh: exercice2

--- app/src/main/java/com/example/td3/MainActivity.java
+++ app/src/main/java/com/example/td3/MainActivity.java
@@ -13,12 +13,14 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
      Button lancement_de;
      TextView affichage_de;
+     TextView deuxieme_de;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         affichage_de = findViewById(R.id.affiche_valeur_de);
+        deuxieme_de = findViewById(R.id.deuxieme_de);
 
         lancement_de = findViewById(R.id.button_lacement_de);
         lancement_de.setOnClickListener(new View.OnClickListener() {
@@ -29,8 +31,13 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT);
                 toast.show();
 
-                int rand = (int)(6.0 * Math.random()) + 1;
-                affichage_de.setText(" "+rand);
+                int rand1 = (int)(6.0 * Math.random()) + 1;
+                affichage_de.setText(Integer.toString(rand1));
+
+                int rand2 = (int)(6.0 * Math.random()) + 1;
+                deuxieme_de.setText(Integer.toString(rand2));
+
+
             }
         });
     }
